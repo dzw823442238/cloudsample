8b9c7a17c186ec9e800ea7010fb5d1b409300f9e
邓志伟 <zhiwei.deng@bqjr.cn>
2018-12-28 11:40:53 +0800

    sample cloud 提交

---
+++ sample-config-client/src/main/java/com/cloud/sample/SampleConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleConfigClientApplication.class, args);
+	}
+}
